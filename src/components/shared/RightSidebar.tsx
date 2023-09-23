b3fa1862e252b208f04381187811388af2696ec1
mohammad amin ghorbani <63232995+magh20@users.noreply.github.com>
2023-09-23 23:09:18 +0330

    finish app

--- src/components/shared/RightSidebar.tsx
+++ src/components/shared/RightSidebar.tsx
@@ -15,7 +15,7 @@ async function RightSidebar() {
     <section className='custom-scrollbar rightsidebar'>
       <div className='flex flex-1 flex-col justify-start'>
         <h3 className='text-heading4-medium text-light-1'>Suggested Communities</h3>
-        <div className='mt-7 flex w-[250px] flex-col gap-10'>
+        <div className='mt-7 flex w-[275px] flex-col gap-10'>
           {suggestedCommunities.communities.length > 0 ? (
             <>
               {suggestedCommunities.communities.map((community) => (
@@ -39,7 +39,7 @@ async function RightSidebar() {
 
       <div className='flex flex-1 flex-col justify-start'>
         <h3 className='text-heading4-medium text-light-1'>Suggested Users</h3>
-        <div className='mt-7 flex w-[250px] flex-col gap-10'>
+        <div className='mt-7 flex w-[275px] flex-col gap-10'>
           {suggestedUsers.users.length > 0 ? (
             <>
               {suggestedUsers.users.map((person) => (
