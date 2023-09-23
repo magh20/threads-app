c0a041ead0d91f0bc659acb6f1c5cf5b2fa694f8
mohammad amin ghorbani <63232995+magh20@users.noreply.github.com>
2023-09-23 22:13:30 +0330

    edit rightsidebar

--- src/components/shared/RightSidebar.tsx
+++ src/components/shared/RightSidebar.tsx
@@ -16,7 +16,7 @@ async function RightSidebar() {
       <div className='flex flex-1 flex-col justify-start'>
         <h3 className='text-heading4-medium text-light-1'>Suggested Communities</h3>
 
-        <div className='mt-7 flex w-[275px] flex-col gap-9'>
+        <div className='mt-7 flex w-[350px] flex-col gap-9'>
           {suggestedCOmmunities.communities.length > 0 ? (
             <>
               {suggestedCOmmunities.communities.map((community) => (
@@ -40,7 +40,7 @@ async function RightSidebar() {
 
       <div className='flex flex-1 flex-col justify-start'>
         <h3 className='text-heading4-medium text-light-1'>Suggested Users</h3>
-        <div className='mt-7 flex w-[275px] flex-col gap-10'>
+        <div className='mt-7 flex w-[350px] flex-col gap-10'>
           {similarMinds.users.length > 0 ? (
             <>
               {similarMinds.users.map((person) => (
