1d7a0f9298dde46ee857f2bb00300379d403c3eb
mohammad amin ghorbani <63232995+magh20@users.noreply.github.com>
2023-09-23 21:31:44 +0330

    edit right sidebar

--- src/components/shared/RightSidebar.tsx
+++ src/components/shared/RightSidebar.tsx
@@ -39,8 +39,8 @@ async function RightSidebar() {
       </div>
 
       <div className='flex flex-1 flex-col justify-start'>
-        <h3 className='text-heading4-medium text-light-1'>Similar Minds</h3>
-        <div className='mt-7 flex w-[350px] flex-col gap-10'>
+        <h3 className='text-heading4-medium text-light-1'>Suggested Users</h3>
+        <div className='mt-7 flex w-[320px] flex-col gap-10'>
           {similarMinds.users.length > 0 ? (
             <>
               {similarMinds.users.map((person) => (
